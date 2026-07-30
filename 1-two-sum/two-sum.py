4ca7e29cbcb9d9e1bca0e8baed5272d333e1ca42
Prajwal <prajwalrujire@gmail.com>
2026-07-30 22:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (40.28%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        set={}
+        d={}
         for i in range(0,len(nums)):
             compliment=target-nums[i]
-            if compliment in set:
-                return set[compliment],i
-            set[nums[i]]=i
+            if compliment in d:
+                return [d[compliment],i]
+            else:
+                d[nums[i]]=i
         return []
